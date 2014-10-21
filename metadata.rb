1730f68654a6638fd181351ef527dfb451640618
Noel King <noelking@gmail.com>
2014-10-21 01:55:30 +0100

    Added Coolest Projects mysql cookbook wrapper

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'cp_mysql'
+maintainer       'Coolest Projects'
+maintainer_email 'coolestprojects@coderdojo.com'
+description      'Installs and configures our mysql'
+version          '0.0.1'
+
+recipe 'default', 'Installs mysql and runs custom recipes'
+
+depends 'mysql'
