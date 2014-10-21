4ae8b05c93fb84ff9cf35559aa63ef225cd889d7
Noel King <noelking@gmail.com>
2014-10-21 01:59:03 +0100

    Fixed typo in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'coolestprojects@coderdojo.com'
 description      'Installs and configures our mysql'
 version          '0.0.1'
 
-recipe 'default', 'Installs mysql and runs custom recipes'
+recipe 'default', 'Installs mysql and runs custom script'
 
 depends 'mysql'
